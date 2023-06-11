7325a601a1b5e4c7fc64770d56079752dd4cbd16
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-11 14:59:39 +0200

    Exercice finish, using a for loop to remove value from array and ignore it if the value doesn't exist

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    for (let i = 0; i < theArgs.length; i++) {
+        const arrayIndex = array.indexOf(theArgs[i]);
+        if (array.includes(theArgs[i])) {
+            array.splice(arrayIndex, 1);
+        }
+        
+    }
+    
+    return array;
 
 };
 

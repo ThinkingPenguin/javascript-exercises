3ae96019b3c85a21c52ad97556c49ec273564ee6
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-11 18:02:52 +0200

    Modified funtion to make sum of all number between two given number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startNum, lastNum) {
+    let total = 0;
+    if (typeof startNum !== 'number' || typeof lastNum !== 'number') {
+        return 'ERROR';   
+    }
+    if (startNum < 0 || lastNum < 0) return 'ERROR';
+
+    if (startNum > lastNum) {
+        for (let i = lastNum; i <= startNum ; i++) {
+            total += i;      
+        }
+        return total
+        
+    } else {
+        for (let i = startNum; i <= lastNum; i++) {
+        total += i;
+    }
+    return total;
+        
+    }
+    
 
 };
 

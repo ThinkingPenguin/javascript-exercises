9f26664271cf3cd957db67a0fd9f2dcbde46ad0e
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-11 18:56:51 +0200

    Finish exercice degree to farheneit and farheneit to degree using Math.round method to round to 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let degreeFahr = (fahrenheit - 32) * 5/9;
+  return Math.round(degreeFahr*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let degreeCelc = (celsius*9/5) + 32;
+  return Math.round(degreeCelc*10)/10;
 };
 
 // Do not edit below this line

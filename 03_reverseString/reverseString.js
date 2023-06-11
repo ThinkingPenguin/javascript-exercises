1f71da4e3f720ba26d54dc332c8b3c7578f8d3e0
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-11 13:45:24 +0200

    Exercice finished for string reverse by splitting a string into an array and making a reverse for loop to concatenate the character from the end of the array inside a new variable

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let charArray = string.split('');
+    let reverse = ''
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverse += charArray[i];
+        
+    }
+    return reverse;
 
 };
 

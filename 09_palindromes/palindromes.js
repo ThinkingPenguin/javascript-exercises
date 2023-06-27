6d8931d70cb46a9ec279a2b6075eadb5a1ed6b71
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-27 22:23:45 +0200

    Exercice done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (stringMan) {
+    let re = /[\W_]/g;
 
+    let lowRegStr = stringMan.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join('');
+
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line

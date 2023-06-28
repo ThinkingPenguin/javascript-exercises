d5df087eee64e162b2581c4237862028f86825ac
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-28 23:15:58 +0200

    finished exercice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(numb) {
+    let numbOne = 0;
+    let numbTwo = 1;
+    let sum = 0;
+    let arrayFib = [];
+    if (numb < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 0; i <= numb; i++) {
+        if (i == 0) {
+            arrayFib.push(1);
+        }
+        sum = numbOne + numbTwo;
+        arrayFib.push(sum);
+        numbOne = numbTwo;
+        numbTwo = sum;
+    }
+    return arrayFib[numb-1];
 
 };
 
